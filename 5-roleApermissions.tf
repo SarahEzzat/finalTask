116c1489c4deeb9f1c947a454ecf6e5e786594c6
SarahRefaat <SarahRefaatsss@gmail.com>
2021-05-19 02:49:33 +0000

    adding role for ec2 nodes and permissions

--- 5-roleApermissions.tf
+++ 5-roleApermissions.tf
@@ -2,13 +2,13 @@
 #   name = "example"
 # }
 
-
+#-------------- this role for k8s cluster
 resource "aws_iam_role" "cluser-role" {
   name = "eks-cluster-role"
 
   assume_role_policy = <<POLICY
 {
-  "Version": "",
+  "Version": "2012-10-17",
   "Statement": [
     {
       "Effect": "Allow",
@@ -21,8 +21,23 @@ resource "aws_iam_role" "cluser-role" {
 }
 POLICY
 }
+#---------------- this to create role for ec2 nodes
+resource "aws_iam_role" "k8s-ec2" {
+  name = "eks-node-group-example"
 
-#-------------- adding permissions
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+#-------------- adding permissions to cluster k8s
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy-attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -41,5 +56,26 @@ resource "aws_iam_role_policy_attachment" "CloudWatchAgentServerPolicy-attach" {
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy-attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.cluser-role.name
+  role       = aws_iam_role.k8s-ec2.name
+}
+
+#--------------- adding permissions to ex2 role of k8s cluster
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.k8s-ec.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.k8s-ec2.name
+}
+
+resource "aws_iam_role_policy_attachment" "CloudWatchAgentServerPolicy-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+  role       = aws_iam_role.k8s-ec2.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.k8s-ec2.name
 }
