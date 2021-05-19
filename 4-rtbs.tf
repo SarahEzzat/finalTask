1ee848f39c883989e55130cbab5178cf1a312ee8
SarahRefaat <SarahRefaatsss@gmail.com>
2021-05-19 00:31:27 +0000

    public route table and assosication of subnets

---
+++ 4-rtbs.tf
@@ -0,0 +1,25 @@
+resource "aws_route_table" "rtbs_pub" {
+  vpc_id = aws_vpc.k8s.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.k8s_igw.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+#---------------- this is assosiation of subnets to routetables
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.sub1-k8s.id
+  route_table_id = aws_route_table.rtbs_pub.id
+}
+
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.sub2-k8s.id
+  route_table_id = aws_route_table.rtbs_pub.id
+}
