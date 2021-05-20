9b39d5f255bb1d865b78049e8342ea3b3d95e894
SarahRefaat <SarahRefaatsss@gmail.com>
2021-05-20 21:51:48 +0000

    last edit insha'allah y3ni

--- 8-nodes.tf
+++ 8-nodes.tf
@@ -8,7 +8,7 @@ resource "aws_eks_node_group" "k8s-ng" {
   instance_types  = ["${var.ins_type}"]
   remote_access {
     ec2_ssh_key     = aws_key_pair.kp.key_name
-    source_security_group_ids = [aws_security_group.k8s-nodes-sg.id]   
+#    source_security_group_ids = [aws_security_group.k8s-nodes-sg.id]   
   }
  
   scaling_config {
--- sg.tf
+++ sg.tf
@@ -14,6 +14,14 @@ resource "aws_security_group" "k8s-nodes-sg" {
   ingress {
     description      = "allow tcp 80"
     from_port        = 0
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description      = "allow tcp 8080"
+    from_port        = 0
     to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
