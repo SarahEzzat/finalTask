e890f731992dd03726d29875b8848fe0abf26a40
SarahRefaat <65432886+SarahRefaat@users.noreply.github.com>
2021-05-21 22:38:55 +0200

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -26,6 +26,14 @@ resource "aws_security_group" "k8s-nodes-sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+  
+   ingress {
+    description      = "allow internals"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["10.0.0.0/16"]
+  }
 
   egress {
     from_port        = 0
